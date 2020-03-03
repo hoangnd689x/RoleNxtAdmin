4f8d6f7858baa5857f0089653031b27040984111
Hoang <hoang.nguyenduy2@vn.bosch.com>
2020-03-03 18:18:20 +0700

    added js file and update excel data

--- src/main/java/com/orgchart/orgchart/controller/RestApiController.java
+++ src/main/java/com/orgchart/orgchart/controller/RestApiController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ import com.orgchart.orgchart.serviceImpl.StructureServiceImpl;
  * @author YOG1HC
  *
  */
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
